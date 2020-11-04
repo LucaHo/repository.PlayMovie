0b41ada2d2b38acf7836da45e5c75d93efde1457
LucaHo <73917079+LucaHo@users.noreply.github.com>
2020-11-04 22:46:06 +0100

    Update _Edit.py

--- plugin.video.PlayMovie.de/_Edit.py
+++ plugin.video.PlayMovie.de/_Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
 MainBase = 'https://ia601509.us.archive.org/14/items/playmovie_Ltd/home.txt'
-addon = xbmcaddon.Addon('plugin.video.PlayMovie')
+addon = xbmcaddon.Addon('plugin.video.PlayMovie.de')
